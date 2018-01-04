048e93e42555135cb19c78871a691832e1877bbb
ssumithra <teelabapple@TEEs-MacBook-Air.local>
2018-01-04 23:53:13 +0530

    Revert "bugfix"
    This reverts commit f2a586ab62235ebd2d37edae0a0a6d9daefa3cc0.

--- PatchSize&matrices.m
+++ PatchSize&matrices.m
@@ -1,10 +1,10 @@
-N=1024 %system size
-for q=0.92%0:0.00001:0.99999
-INP=['rep10tcp_q',num2str(q),'_spatial_data_2852a.dat']; %set 0s according to resolution of q in simulations
-k1=dlmread(INP);
+N=1024 #system size
+for q=0%0:0.00001:0.99999
+INP=['tcp_q',num2str(q),'_spatial_data_PD.dat']; %set 0s according to resolution of q in simulations
+k=dlmread(INP);
 for j=999999:-1:1  %set according to resolution of p in the simulations
 psd=0;
-n=50 %this is the number of replicates
+n=50 #this is the number of replicates
 for i=1:n
 kmat=reshape(k((999999-j)*N*N*n+((i-1)*N*N)+1:(999999-j)*N*N*n+(i*N*N)),N,N); %again modify according to resolution
 csvwrite(['q',num2str(q),'p',num2str(j/1000000),'patch',num2str(i),'.csv'],kmat)

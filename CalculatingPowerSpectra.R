981bfce8477102910bab3f83b58f3f9b5ae6212f
ssumithra <teelabapple@TEEs-MacBook-Air.local>
2018-01-05 00:00:25 +0530

    bugfix
    Fixed small bugs in the Patchsize&matrices (line 7) and
    CalculatingPowerSpectra (Line 6) codes

--- CalculatingPowerSpectra.R
+++ CalculatingPowerSpectra.R
@@ -3,5 +3,5 @@ p= 0.7225 #the p value of the simulations that produced the set of realisations
 Gallpatches<-list.files(getwd(),recursive=TRUE)
 Gallpatches
 GPSpecq0p62275<-lapply(Gallpatches,function(x){read.csv(x,header=TRUE)})
-GPSpec<-lapply(GPSpecq0p62275,function(x){x[,"x"]})
+GPSpec<-lapply(GPSpecq0p62275,function(x){rspec(x)[[2]]})
 for i in (1:length(GPSpec)){write.csv(GPSpec[[i]],paste("PowSpecq",q,"p",p,"patch",i,".csv",sep=""))}
--- PatchSize&matrices.m
+++ PatchSize&matrices.m
@@ -4,7 +4,7 @@ INP=['tcp_q',num2str(q),'_spatial_data_PD.dat']; %set 0s according to resolution
 k=dlmread(INP);
 for j=999999:-1:1  %set according to resolution of p in the simulations
 psd=0;
-n=50 #this is the number of replicates
+n=50 %this is the number of replicates
 for i=1:n
 kmat=reshape(k((999999-j)*N*N*n+((i-1)*N*N)+1:(999999-j)*N*N*n+(i*N*N)),N,N); %again modify according to resolution
 csvwrite(['q',num2str(q),'p',num2str(j/1000000),'patch',num2str(i),'.csv'],kmat)
